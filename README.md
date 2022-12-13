e9b9746a0184edc11ed0b58b88f91bebac83efd9
RuinedYourLife <debhi.quentin@gmail.com>
2022-12-13 20:47:50 +0000

    setup: base architecture

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Advent Of Code
 
 me learning my way through cool looking languages with aoc (:
+
+## Tools
+
+[aoc-cli](https://github.com/scarvalhojr/aoc-cli)
+
+## Languages
+
+Elixir
---
+++ elixir/mix.exs
@@ -0,0 +1,29 @@
+defmodule Advent.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :dev,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:jason, "~> 1.2"},
+      {:libgraph, "~> 0.13.3"},
+      {:benchee, "~> 1.0"}
+    ]
+  end
+end
